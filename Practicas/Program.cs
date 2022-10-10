d6768c461807fe5148272d7d2de5ed7fcbbec8bf
Sergio Morales García <moralesgarcia300@gmail.com>
2022-10-10 13:06:10 +0200

    Área de un círcculo

--- Practicas/Program.cs
+++ Practicas/Program.cs
@@ -10,6 +10,19 @@ namespace Practicas
     {
         static void Main(string[] args)
         {
+            // Ejercicio del área del círculo
+            int edad = 25;
+            int alumnos = 24;
+            string curso = "DEINT";
+
+            ///Console.WriteLine($"Tu edad es de {edad}");
+            ///Console.WriteLine("edad: {0}", edad);
+            // Console.WriteLine("Tu edad es de {0}", edad);
+            //Console.WriteLine("En la clase de {0} hay {1} alumnos", curso, alumnos);
+            Console.WriteLine("Intoduzca un número", curso, alumnos);
+            int radio = int.Parse(Console.ReadLine());
+            double area = Math.Pow(radio, 2) * Math.PI;
+            Console.WriteLine($"El área del círculo de radio {radio} es {area}");
         }
     }
 }
