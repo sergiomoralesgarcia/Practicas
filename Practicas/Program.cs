58c2f5be1ae7f7d865c21d846726394a5fc59fef
Sergio Morales García <moralesgarcia300@gmail.com>
2022-10-10 14:31:59 +0200

    Añadida práctica 2

--- Practicas/Program.cs
+++ Practicas/Program.cs
@@ -10,6 +10,15 @@ namespace Practicas
     {
         static void Main(string[] args)
         {
+            //Practica1();
+            Practica2();
+
+        }
+
+        #region Practica 1 Area Circulo
+        static void Practica1()
+        {
+            
             // Ejercicio del área del círculo
             int edad = 25;
             int alumnos = 24;
@@ -23,6 +32,41 @@ namespace Practicas
             int radio = int.Parse(Console.ReadLine());
             double area = Math.Pow(radio, 2) * Math.PI;
             Console.WriteLine($"El área del círculo de radio {radio} es {area}");
+
+            
+
         }
+        #endregion
+
+        #region
+        static void Practica2()
+        {
+            const int PUEDE_CONDUCIR = 18;
+            const string TIENE_CARNET = "S";
+
+            Console.WriteLine("Vamos a evaluar si puedes conducir");
+            Console.WriteLine("Introduce tu edad:");
+            int edad = Int32.Parse(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("¿Tienes carnet?(S/N)");
+                string carnet = Console.ReadLine().ToUpper();
+                if (carnet == TIENE_CARNET)
+                {
+                    Console.WriteLine("Puedes conducir");
+                }
+                else
+                {
+                    Console.WriteLine("No puedes conducir");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("No puedes conducir");
+            }
+        }
+        #endregion
     }
 }
