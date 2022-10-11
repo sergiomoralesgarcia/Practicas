80d4e10fa27e0851994635daf3ae779e5663167b
Sergio Morales García <moralesgarcia300@gmail.com>
2022-10-11 14:44:11 +0200

    Practica 3

--- Practicas/Program.cs
+++ Practicas/Program.cs
@@ -11,7 +11,8 @@ namespace Practicas
         static void Main(string[] args)
         {
             //Practica1();
-            Practica2();
+            //Practica2();
+            Practica3();
 
         }
 
@@ -38,8 +39,39 @@ namespace Practicas
         }
         #endregion
 
-        #region
+        #region Practica 2 comprobar si puedes conducir
         static void Practica2()
+        {
+            const int PUEDE_CONDUCIR = 18;
+            const string TIENE_CARNET = "S";
+
+            Console.WriteLine("Vamos a evaluar si puedes conducir");
+            Console.WriteLine("Introduce tu edad:");
+            int edad = Int32.Parse(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("¿Tienes carnet?(S/N)");
+                string carnet = Console.ReadLine().ToUpper();
+                if (carnet == TIENE_CARNET)
+                {
+                    Console.WriteLine("Puedes conducir");
+                }
+                else
+                {
+                    Console.WriteLine("No puedes conducir");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("No puedes conducir");
+            }
+        }
+        #endregion
+
+        #region Practica 3 comprobar si S/N está bien introducido
+        static void Practica3()
         {
             const int PUEDE_CONDUCIR = 18;
             const string TIENE_CARNET = "S";
